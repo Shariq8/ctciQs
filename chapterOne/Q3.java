72337ce185fdc0c6330b417f7dec3835cba0183a
Shariq A <shariq7991@gmail.com>
2022-08-20 00:53:28 +0100

    q3

---
+++ chapterOne/Q3.java
@@ -0,0 +1,27 @@
+public class Q3 {
+    public static void main(String args[]){
+        System.out.println(URLify("Mr John Smith    ", 13));
+    }
+
+    public static String URLify(String s, int length){
+        //Run time: O(N)
+        // char[] chars = s.toCharArray();
+        // String result = "";
+        // for(int i = 0; i < length; i++ ){
+        //     if(chars[i] == ' '){
+        //         result+="%20";
+        //     }else{
+        //         result+=chars[i];
+        //     }
+        // }
+        // return result;
+
+        //Book solution:
+        
+    }
+
+    public static void replaceSpaces(char[] str, int trueLength){
+        int spaceCount = 0, index, i=0;
+        for(int i = 0; i <0)
+    }
+}
