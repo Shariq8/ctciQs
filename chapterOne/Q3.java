0dbf706acfe247a869c2739c5b05c963f0bd452c
Shariq A <shariq7991@gmail.com>
2022-08-22 11:43:34 +0100

    q3

--- chapterOne/Q3.java
+++ chapterOne/Q3.java
@@ -5,23 +5,15 @@ public class Q3 {
 
     public static String URLify(String s, int length){
         //Run time: O(N)
-        // char[] chars = s.toCharArray();
-        // String result = "";
-        // for(int i = 0; i < length; i++ ){
-        //     if(chars[i] == ' '){
-        //         result+="%20";
-        //     }else{
-        //         result+=chars[i];
-        //     }
-        // }
-        // return result;
-
-        //Book solution:
-        
-    }
-
-    public static void replaceSpaces(char[] str, int trueLength){
-        int spaceCount = 0, index, i=0;
-        for(int i = 0; i <0)
+        char[] chars = s.toCharArray();
+        String result = "";
+        for(int i = 0; i < length; i++ ){
+            if(chars[i] == ' '){
+                result+="%20";
+            }else{
+                result+=chars[i];
+            }
+        }
+        return result;
     }
 }
