23d62c4e19a7e8affa894b5059e673c38de260c8
Shariq A <shariq7991@gmail.com>
2022-08-23 12:17:01 +0100

    Q1

--- chapterTwo/Q1.java
+++ chapterTwo/Q1.java
@@ -12,25 +12,43 @@ public class Q1{
       list.add(4);
       list.add(8);
       System.out.println(list.toString());
-      deleteDups(list);
+      System.out.println(deleteDups(list).toString());
     }
 
-    public static MyLinkedList<Integer> deleteDups(MyLinkedList<Integer> list){
-      //Idea - iterate through the list, see if the hashset already contains the value, if so, remove it. Otherwise continue
-      HashSet<Integer> set = new HashSet<Integer>();
-      Node<Integer> previous = new Node<>(null);
-      Node<Integer> ptr = list.head;
-      while(ptr != null){
-        if(set.contains(ptr.getValue())){
-          previous.setNext(ptr.getNext()); 
-        }else{
-          set.add(ptr.getValue());
-          previous = ptr;
-        }
-        ptr = ptr.getNext();
-      }
+    //O(N) time but also O(N) space
+    // public static MyLinkedList<Integer> deleteDups(MyLinkedList<Integer> list){
+    //   //Idea - iterate through the list, see if the hashset already contains the value, if so, remove it. Otherwise continue
+    //   HashSet<Integer> set = new HashSet<Integer>();
+    //   Node<Integer> previous = new Node<>(null);
+    //   Node<Integer> ptr = list.head;
+    //   while(ptr != null){
+    //     if(set.contains(ptr.getValue())){
+    //       previous.setNext(ptr.getNext()); 
+    //     }else{
+    //       set.add(ptr.getValue());
+    //       previous = ptr;
+    //     }
+    //     ptr = ptr.getNext();
+    //   }
 
-      System.out.println(list.toString());
+    //   System.out.println(list.toString());
+    //   return list;
+    // }
+
+    //No Buffer deletion O(N^2) time but O(1) space
+    public static MyLinkedList<Integer> deleteDups(MyLinkedList<Integer> list){
+      Node<Integer> current = list.head;
+      while(current != null){
+        Node<Integer> runner = current;
+        while(runner.getNext() != null){
+          if(runner.getNext().getValue() == current.getValue()){
+            runner.setNext(runner.getNext().getNext());
+          }else{
+            runner = runner.getNext();
+          }
+        }
+        current = current.getNext();
+      }
       return list;
     }
 }
