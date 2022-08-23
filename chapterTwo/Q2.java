4d30049388ed04e95c01df320507f9a76415d246
Shariq A <shariq7991@gmail.com>
2022-08-23 12:44:20 +0100

    Q2

--- chapterTwo/Q2.java
+++ chapterTwo/Q2.java
@@ -1,5 +1,27 @@
 public class Q2{
     public static void main(String[] args){
-      System.out.println("Hello World"); 
+      MyLinkedList<Integer> list = new MyLinkedList<>();
+      list.add(2);
+      list.add(4);
+      list.add(6);
+      list.add(4);
+      list.add(5);
+      list.add(9);
+      System.out.println(list.toString());
+      System.out.println(kThNode(list, 3));
+    }
+
+
+    public static Integer kThNode(MyLinkedList<Integer> list, int k){
+      if(list.head == null){
+        return 0;
+      }
+
+      int index = list.size() - k, count = 0;
+      while(count != index){
+        list.head = list.head.getNext();
+        count++;
+      }
+      return list.head.getValue();
     }
 }
