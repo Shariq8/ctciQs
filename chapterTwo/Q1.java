ab72edef76e74b8f3c0f829be30483a839ec7702
Shariq A <shariq7991@gmail.com>
2022-08-23 12:11:45 +0100

    Q

--- chapterTwo/Q1.java
+++ chapterTwo/Q1.java
@@ -3,6 +3,7 @@ import java.util.HashSet;
 public class Q1{
     public static void main(String[] args){
       MyLinkedList<Integer> list = new MyLinkedList<>();
+      list.add(3);
       list.add(2);
       list.add(5);
       list.add(6);
